0982777e5aea8bdb49d568a871a9f318fa7e1aec
Vlad Grigoret <93397531+arcanecord@users.noreply.github.com>
2023-06-30 15:26:59 +0200

    Add small validation

--- main.go
+++ main.go
@@ -12,18 +12,19 @@ import (
 	"github.com/gin-gonic/gin"
 	_ "github.com/mattn/go-sqlite3"
 )
+
 func createTable(db *sql.DB) {
-    urls_table := `CREATE TABLE urls(
+	urls_table := `CREATE TABLE urls(
         id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-			  "short_url" TEXT,
+		"short_url" TEXT,
         "original_url" TEXT);`
 
-        query, err := db.Prepare(urls_table)
-        if err != nil {
-            log.Fatal(err)
-        }
-        query.Exec()
-        fmt.Println("Table created successfully!")
+	query, err := db.Prepare(urls_table)
+	if err != nil {
+		log.Fatal(err)
+	}
+	query.Exec()
+	fmt.Println("Table created successfully!")
 }
 func addUrl(db *sql.DB, short_url string, original_url string) {
 	insert_url := `INSERT INTO urls(short_url, original_url) VALUES (?, ?)`
@@ -32,7 +33,7 @@ func addUrl(db *sql.DB, short_url string, original_url string) {
 		log.Fatal(err)
 	}
 	query.Exec(short_url, original_url)
-	fmt.Println("Url added successfully!")
+	fmt.Println("Url added successfully! ", short_url, " ", original_url)
 }
 func getUrl(db *sql.DB, short_url string) string {
 	var original_url string
@@ -46,43 +47,50 @@ func createShortUrl(original_url string) string {
 	h := sha256.New()
 	h.Write([]byte(original_url))
 	hashed_url := fmt.Sprintf("%x", h.Sum(nil))
-	return hashed_url[:8] 
+	return hashed_url[:8]
 }
-func main(){
+func main() {
 
-if _, err := os.Stat("database.db"); os.IsNotExist(err) {
-	file, err := os.Create("database.db")
-	if err != nil {
-		log.Fatal(err)
+	if _, err := os.Stat("database.db"); os.IsNotExist(err) {
+		file, err := os.Create("database.db")
+		if err != nil {
+			log.Fatal(err)
+		}
+		db, err := sql.Open("sqlite3", "./database.db")
+		if err != nil {
+			log.Fatal(err)
+		}
+		createTable(db)
+		file.Close()
 	}
-	db, err := sql.Open("sqlite3", "./database.db")
-	if err != nil {
-		log.Fatal(err)
-	}
-	createTable(db)
-	file.Close()
-}
-	
-  r := gin.Default()
-  r.GET("/", func(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-	  "message": "Hello World!",
+
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello World!",
+		})
 	})
-  })
-  r.POST("/shorten", func(c *gin.Context) {
-	original_url := c.PostForm("url")
-	db, err := sql.Open("sqlite3", "./database.db")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-	short_url := createShortUrl(original_url)
-	addUrl(db, short_url, original_url)
-	c.JSON(http.StatusOK, gin.H{
-		"short_url": short_url,
+	r.POST("/shorten", func(c *gin.Context) {
+		original_url := c.PostForm("url")
+		if original_url == "" {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "Url is empty",
+			})
+			return
+		}
+
+		db, err := sql.Open("sqlite3", "./database.db")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer db.Close()
+		short_url := createShortUrl(original_url)
+		addUrl(db, short_url, original_url)
+		c.JSON(http.StatusOK, gin.H{
+			"short_url": short_url,
+		})
 	})
-	  })
-	    r.GET("/:short_url", func(c *gin.Context) {
+	r.GET("/:short_url", func(c *gin.Context) {
 		short_url := c.Param("short_url")
 		db, err := sql.Open("sqlite3", "./database.db")
 		if err != nil {
@@ -93,6 +101,5 @@ if _, err := os.Stat("database.db"); os.IsNotExist(err) {
 		c.Redirect(http.StatusMovedPermanently, original_url)
 	})
 
-
-  r.Run() // listen and serve on
+	r.Run() // listen and serve on
 }
